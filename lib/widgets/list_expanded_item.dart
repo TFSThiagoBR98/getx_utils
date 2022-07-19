edbc52df41a81c87c09b74f49ff54cae9f6410aa
Thiago França da Silva <tfsthiagobr98@outlook.com>
2022-07-19 17:38:37 -0300

    feat: allow disable action list

--- lib/widgets/list_expanded_item.dart
+++ lib/widgets/list_expanded_item.dart
@@ -5,12 +5,22 @@ class ListExpandedItem extends StatelessWidget {
   final String name;
   final Widget? leading;
   final ImageProvider<Object>? image;
+  final bool hideTrailing;
+  final bool hideChildren;
 
   final ValueChanged<String>? onEdit;
   final ValueChanged<String>? onDelete;
 
   const ListExpandedItem(
-      {Key? key, required this.id, required this.name, this.onEdit, this.onDelete, this.leading, this.image})
+      {Key? key,
+      required this.id,
+      required this.name,
+      this.onEdit,
+      this.onDelete,
+      this.leading,
+      this.image,
+      this.hideTrailing = false,
+      this.hideChildren = false})
       : super(key: key);
 
   @override
@@ -22,69 +32,73 @@ class ListExpandedItem extends StatelessWidget {
         style: Theme.of(context).textTheme.headline6,
       ),
       tilePadding: const EdgeInsets.all(14.0),
-      trailing: SizedBox(
-        width: 125,
-        child: ButtonBar(
-          children: [
-            IconButton(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                iconSize: 32,
-                onPressed: onEdit != null ? () => onEdit!(id) : null,
-                icon: const Icon(Icons.edit_outlined)),
-            IconButton(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              iconSize: 32,
-              onPressed: onDelete != null ? () => onDelete!(id) : null,
-              icon: const Icon(Icons.delete_outline_rounded),
-            )
-          ],
-        ),
-      ),
-      children: [
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.transparent,
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.all(8),
-                  height: 70,
-                  child: ElevatedButton.icon(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<OutlinedBorder?>(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      )),
-                    ),
-                    onPressed: onEdit != null ? () => onEdit!(id) : null,
-                    icon: const Icon(Icons.edit_outlined),
-                    label: const Text("Editar"),
-                  ),
-                ),
-                Container(
-                  color: Colors.transparent,
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.all(8),
-                  height: 70,
-                  child: ElevatedButton.icon(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<OutlinedBorder?>(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      )),
-                    ),
+      trailing: hideTrailing
+          ? const SizedBox.shrink()
+          : SizedBox(
+              width: 125,
+              child: ButtonBar(
+                children: [
+                  IconButton(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      iconSize: 32,
+                      onPressed: onEdit != null ? () => onEdit!(id) : null,
+                      icon: const Icon(Icons.edit_outlined)),
+                  IconButton(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    iconSize: 32,
                     onPressed: onDelete != null ? () => onDelete!(id) : null,
-                    icon: const Icon(Icons.delete_outline),
-                    label: const Text("Apagar"),
+                    icon: const Icon(Icons.delete_outline_rounded),
+                  )
+                ],
+              ),
+            ),
+      children: hideChildren
+          ? []
+          : [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        color: Colors.transparent,
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.all(8),
+                        height: 70,
+                        child: ElevatedButton.icon(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<OutlinedBorder?>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            )),
+                          ),
+                          onPressed: onEdit != null ? () => onEdit!(id) : null,
+                          icon: const Icon(Icons.edit_outlined),
+                          label: const Text("Editar"),
+                        ),
+                      ),
+                      Container(
+                        color: Colors.transparent,
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.all(8),
+                        height: 70,
+                        child: ElevatedButton.icon(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<OutlinedBorder?>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            )),
+                          ),
+                          onPressed: onDelete != null ? () => onDelete!(id) : null,
+                          icon: const Icon(Icons.delete_outline),
+                          label: const Text("Apagar"),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-      ],
+              ),
+            ],
     );
   }
 }
--- lib/widgets/list_expanded_item_view.dart
+++ lib/widgets/list_expanded_item_view.dart
@@ -5,12 +5,22 @@ class ListExpandedItemView extends StatelessWidget {
   final String name;
   final Widget? leading;
   final ImageProvider<Object>? image;
+  final bool hideTrailing;
+  final bool hideChildren;
 
   final ValueChanged<String>? onEdit;
   final ValueChanged<String>? onPayment;
 
   const ListExpandedItemView(
-      {Key? key, required this.id, required this.name, this.onEdit, this.onPayment, this.leading, this.image})
+      {Key? key,
+      required this.id,
+      required this.name,
+      this.onEdit,
+      this.onPayment,
+      this.leading,
+      this.image,
+      this.hideTrailing = false,
+      this.hideChildren = false})
       : super(key: key);
 
   @override
@@ -22,69 +32,73 @@ class ListExpandedItemView extends StatelessWidget {
         style: Theme.of(context).textTheme.headline6,
       ),
       tilePadding: const EdgeInsets.all(14.0),
-      trailing: SizedBox(
-        width: 125,
-        child: ButtonBar(
-          children: [
-            IconButton(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                iconSize: 32,
-                onPressed: onEdit != null ? () => onEdit!(id) : null,
-                icon: const Icon(Icons.edit_outlined)),
-            IconButton(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              iconSize: 32,
-              onPressed: onPayment != null ? () => onPayment!(id) : null,
-              icon: const Icon(Icons.payment_outlined),
-            )
-          ],
-        ),
-      ),
-      children: [
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  color: Colors.transparent,
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.all(8),
-                  height: 70,
-                  child: ElevatedButton.icon(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<OutlinedBorder?>(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      )),
-                    ),
-                    onPressed: onEdit != null ? () => onEdit!(id) : null,
-                    icon: const Icon(Icons.edit_outlined),
-                    label: const Text("Editar"),
-                  ),
-                ),
-                Container(
-                  color: Colors.transparent,
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.all(8),
-                  height: 70,
-                  child: ElevatedButton.icon(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<OutlinedBorder?>(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      )),
-                    ),
+      trailing: hideTrailing
+          ? const SizedBox.shrink()
+          : SizedBox(
+              width: 125,
+              child: ButtonBar(
+                children: [
+                  IconButton(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      iconSize: 32,
+                      onPressed: onEdit != null ? () => onEdit!(id) : null,
+                      icon: const Icon(Icons.edit_outlined)),
+                  IconButton(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    iconSize: 32,
                     onPressed: onPayment != null ? () => onPayment!(id) : null,
                     icon: const Icon(Icons.payment_outlined),
-                    label: const Text("Pagamentos"),
+                  )
+                ],
+              ),
+            ),
+      children: hideChildren
+          ? []
+          : [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        color: Colors.transparent,
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.all(8),
+                        height: 70,
+                        child: ElevatedButton.icon(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<OutlinedBorder?>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            )),
+                          ),
+                          onPressed: onEdit != null ? () => onEdit!(id) : null,
+                          icon: const Icon(Icons.edit_outlined),
+                          label: const Text("Editar"),
+                        ),
+                      ),
+                      Container(
+                        color: Colors.transparent,
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.all(8),
+                        height: 70,
+                        child: ElevatedButton.icon(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<OutlinedBorder?>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            )),
+                          ),
+                          onPressed: onPayment != null ? () => onPayment!(id) : null,
+                          icon: const Icon(Icons.payment_outlined),
+                          label: const Text("Pagamentos"),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-      ],
+              ),
+            ],
     );
   }
 }
