86082a89b775a548c542ffeed42462be91c4b16f
Thiago França da Silva <tfsthiagobr98@outlook.com>
2022-07-26 09:38:53 -0300

    feat: add money support feat: add timer calculation functions

--- lib/utils/main_utils.dart
+++ lib/utils/main_utils.dart
@@ -1,16 +1,22 @@
+import 'dart:async';
+
 import 'package:decimal/decimal.dart';
 import 'package:decimal/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
+import 'package:money2/money2.dart';
+
+import 'func_utils.dart';
 
 abstract class MainUtils {
   static Future<void> initFlutterApp() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Hive.initFlutter();
     await Hive.openBox("settings");
-    Logger.root.level = Level.ALL; // defaults to Level.INFO
+    Logger.root.level = Level.ALL;
     Logger.root.onRecord.listen((record) {
       debugPrint('${record.level.name}: ${record.time}: ${record.message}');
     });
@@ -26,6 +32,12 @@ abstract class MainUtils {
     symbol: "",
   );
 
+  static final Currency brlCurrency = Currency.create('BRL', 2, symbol: "R\$");
+
+  static String stringDecimalToMoney(String decimalValue) {
+    return decimal2money(Decimal.tryParse(decimalValue) ?? Decimal.zero);
+  }
+
   static Decimal money2decimal(String value) {
     return Decimal.parse(MainUtils.lformatMoney.parse(value).toStringAsFixed(8));
   }
@@ -40,5 +52,30 @@ abstract class MainUtils {
 
   static String decimal2string(Decimal value) {
     return MainUtils.lformatDouble.format(DecimalIntl(value));
+  }
+
+  static Money centsToMoney(int cents) {
+    return Money.fromIntWithCurrency(cents, brlCurrency);
+  }
+
+  static String moneyToString(Money money) {
+    return money.toString();
+  }
+
+  static Money stringToMoney(String money) {
+    return brlCurrency.parse(money);
+  }
+
+  static BigInt moneyToCents(Money money) {
+    return money.minorUnits;
+  }
+
+  static void runWhenContextAvaliable(ContextCallback callback) {
+    Timer.periodic(const Duration(seconds: 1), (Timer t) async {
+      if (Get.context != null) {
+        callback(Get.context!);
+        t.cancel();
+      }
+    });
   }
 }
---
+++ lib/utils/timer_utils.dart
@@ -0,0 +1,110 @@
+import 'package:intl/intl.dart';
+
+import '../app/getx_datetime_controller.dart';
+
+String fetchTimeLeftFromDuration(Duration duration) {
+  var microseconds = duration.inMicroseconds;
+
+  var hours = microseconds ~/ Duration.microsecondsPerHour;
+  microseconds = microseconds.remainder(Duration.microsecondsPerHour);
+
+  if (microseconds < 0) microseconds = -microseconds;
+
+  var minutes = microseconds ~/ Duration.microsecondsPerMinute;
+  microseconds = microseconds.remainder(Duration.microsecondsPerMinute);
+
+  var minutesPadding = minutes < 10 ? "0" : "";
+
+  var seconds = microseconds ~/ Duration.microsecondsPerSecond;
+  microseconds = microseconds.remainder(Duration.microsecondsPerSecond);
+
+  var secondsPadding = seconds < 10 ? "0" : "";
+
+  return "$hours:"
+      "$minutesPadding$minutes:"
+      "$secondsPadding$seconds";
+}
+
+double percentageLeftFromPadding(String startDateTime, String? targetDateTime) {
+  if (targetDateTime != null) {
+    DateTime now = DateTime.now().toUtc();
+    DateTime start = fromInternal(startDateTime, GetXDateTimeControllerDisplayFormat.datetime)!;
+    DateTime end = fromInternal(targetDateTime, GetXDateTimeControllerDisplayFormat.datetime)!;
+    Duration total = end.difference(start);
+    Duration left = end.difference(now);
+    double percentage = (left.inMilliseconds / total.inMilliseconds);
+    return percentage;
+  } else {
+    return 1;
+  }
+}
+
+const String displayDateFormat = "dd/MM/yyyy";
+const String displayTimeFormat = "HH:mm:ss";
+
+const String internalDateFormat = "yyyy-MM-dd";
+const String internalTimeFormat = "HH:mm:ss";
+
+String displayFormat(GetXDateTimeControllerDisplayFormat format) {
+  if (format == GetXDateTimeControllerDisplayFormat.date) {
+    return displayDateFormat;
+  } else if (format == GetXDateTimeControllerDisplayFormat.time) {
+    return displayTimeFormat;
+  } else {
+    return "$displayDateFormat $displayTimeFormat";
+  }
+}
+
+String internalFormat(GetXDateTimeControllerDisplayFormat format) {
+  if (format == GetXDateTimeControllerDisplayFormat.date) {
+    return internalDateFormat;
+  } else if (format == GetXDateTimeControllerDisplayFormat.time) {
+    return internalTimeFormat;
+  } else {
+    return "$internalDateFormat $internalTimeFormat";
+  }
+}
+
+String? toInternalFormat(DateTime? data, GetXDateTimeControllerDisplayFormat format) {
+  if (data == null) return null;
+  try {
+    return DateFormat(internalFormat(format)).format(data.toUtc());
+  } on FormatException {
+    return null;
+  }
+}
+
+String? fromInternalToDisplayFormat(String value, GetXDateTimeControllerDisplayFormat format) {
+  try {
+    DateTime? data = fromInternal(value, format);
+    if (data == null) return null;
+    return DateFormat(displayFormat(format)).format(data.toLocal());
+  } on FormatException {
+    return null;
+  }
+}
+
+String? toDisplayFormat(DateTime? data, GetXDateTimeControllerDisplayFormat format) {
+  if (data == null) return null;
+  try {
+    return DateFormat(displayFormat(format)).format(data);
+  } on FormatException {
+    return null;
+  }
+}
+
+DateTime? fromInternal(String value, GetXDateTimeControllerDisplayFormat format) {
+  try {
+    return DateFormat(internalFormat(format)).parse(value, true);
+  } on FormatException {
+    return null;
+  }
+}
+
+DateTime? fromDisplay(String value, GetXDateTimeControllerDisplayFormat format) {
+  try {
+    return DateFormat(displayFormat(format)).parse(value, false);
+  } on FormatException {
+    return null;
+  }
+}
