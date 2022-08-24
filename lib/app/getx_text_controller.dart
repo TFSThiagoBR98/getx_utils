2d552e2dcffc645b27a3809925bf0900a063c5ee
Thiago França da Silva <tfsthiagobr98@outlook.com>
2022-08-24 11:45:48 -0300

    fix(utils): avoid break GetXTextController set data feature

--- lib/app/getx_text_controller.dart
+++ lib/app/getx_text_controller.dart
@@ -2,28 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class GetXTextController<V> {
-  late final Rxn<V> dataRx;
-  V? get data => dataRx.value;
-  set data(V? value) => dataRx.value = value;
+  final RxnString dataRx = RxnString();
+  String? get data => dataRx.value;
+  set data(String? value) => dataRx.value = value;
 
   final Rx<TextEditingController> _controller = TextEditingController().obs;
   TextEditingController get controller => _controller.value;
   set controller(TextEditingController value) => _controller.value = value;
 
-  void setData(String value) {
-    if (V is String) {
-      data = value as V?;
-    }
-    controller.text = value;
+  void setData(String? value) {
+    data = value;
+    controller.text = value ?? '';
   }
 
-  GetXTextController() {
-    if (V is String) {
-      dataRx = RxnString() as Rxn<V>;
-    } else if (V is num) {
-      dataRx = RxnNum() as Rxn<V>;
-    } else {
-      dataRx = Rxn<V>();
-    }
-  }
+  GetXTextController();
 }
