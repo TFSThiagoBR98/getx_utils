c61e3d581a93aa5e867154904ff4608fdab790f0
Thiago França da Silva <tfsthiagobr98@outlook.com>
2022-08-31 17:40:29 -0300

    fix: add image provider to getx_media

--- lib/media_mngr/getx_media.dart
+++ lib/media_mngr/getx_media.dart
@@ -1,8 +1,10 @@
 import 'package:cross_file/cross_file.dart';
+import 'package:flutter/material.dart';
 
 class GetxMedia<T> {
   final T? model;
   final XFile? file;
+  final ImageProvider<Object>? image;
 
-  const GetxMedia({this.model, this.file});
+  const GetxMedia({this.model, this.file, this.image});
 }
